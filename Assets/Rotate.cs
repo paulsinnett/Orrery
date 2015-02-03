8f20f18aff94d727e22a2907f9d2d28cdea82220
Paul Sinnett <paul.sinnett@gmail.com>
2015-02-03 18:45:12 +0000

    Example code
    Code demonstrated in class

---
+++ Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float speed = 60f;
+	float angle = 0f;
+
+	// Update is called once per frame
+	void Update () {
+
+		angle = Mathf.Repeat (angle + Time.deltaTime * speed, 360f);
+		transform.rotation = Quaternion.Euler (0f, angle, 0f);
+	}
+}
